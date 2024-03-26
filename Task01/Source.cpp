13003494df7b1e307e61c1668688d00f615a0cab
LoJu310 <l.julya.vl0310@gmail.com>
2024-03-26 18:24:15 +0300

    Add project files.

---
+++ Task01/Source.cpp
@@ -0,0 +1,23 @@
+#include "Student.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	Student st1;
+	Student st2;
+	Student st3;
+
+	st1.name = "Alex";
+	st1.age = 14;
+	st1.mark = 9;
+
+	st2.name = "Alice";
+	st2.age = 13;
+	st2.mark = 7;
+
+	st3.name = "Potter";
+	st3.age = 15;
+	st3.mark = 10;
+
+	return 0;
+}
---
+++ Task01/Student.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Student {
+public:
+	string name;
+	int age;
+	double mark;
+};
