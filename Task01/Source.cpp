33b3b87d8ba2015d9271415d39ed3da84c413af2
LoJu310 <l.julya.vl0310@gmail.com>
2024-04-02 18:50:23 +0300

    Add testing code of Manager (тестирование функционала класса Manager)

--- Task01/Source.cpp
+++ Task01/Source.cpp
@@ -1,44 +1,20 @@
-#include "Student.h"
+#include "Manager.h"
 #include <iostream>
 using namespace std;
 
 int main() {
-	//int n{10};
-	//int n = 10;
-	//int array[10]{};
-	//int array[10]{1,2,3,4,5};
-	//int array[4]{1,2,3,4};
+	Student st1("Alex", 14, 9);
+	Student st2("Alice", 13, 7);
+	Student st3("Peter", 13, 6);
+	Student st4("Potter", 13, 10);
+	Student st5("Matvey", 15, 8);
 
-	//for (int i = 0; i < 10; i++)
-	//{
-	//	cout << array[i] << " ";
-	//}
+	Student students[]{ st1, st2, st3, st4, st5 };
 
-	//cout << endl;
+	Manager manager;
 
-	//Student st1("Alex", 14, 9);
-	//Student st2("Alice");
-	//Student st3(10);
-	//Student st4;
-	//Student st5{};
-	//Student st6{"Peter"};
-	//Student st7{ "Anna", 13, 7};
-	////Student st8{ "Anna", 13};	//!!! Compiler Error
-	//Student st9 = Student("Alice");
-
-	const int size = 5;
-	//Student students[size];
-	Student students[size]{ {"Alice"}, {10}, {"Peter", 14, 9}, {"Peter"} ,{9}};
-
-	//cout << st1.convert() << endl;
-	//cout << st2.convert() << endl;
-	//cout << st3.convert() << endl;
-	//cout << st4.convert() << endl;
-	//cout << st5.convert() << endl;
-	//cout << st6.convert() << endl;
-	//cout << st7.convert() << endl;
-	//cout << st8.convert() << endl;
-
+	cout << "Best mark is " << manager.getMaxMark(students, 5) << endl;
+	cout << "Worst mark is " << manager.getMinMark(students, 5) << endl;
 
 	return 0;
 }
