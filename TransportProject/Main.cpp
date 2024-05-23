df5794afefcb970d34df78feef62e46d0f0f0473
ivanvikvik <ivanvikvik@gmail.com>
2024-05-23 18:45:45 +0300

    Use method overriding (использование переопределения методов - истинного полиморфизма в ООП)

--- TransportProject/Main.cpp
+++ TransportProject/Main.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
-#include "Car.h"
-#include "Bus.h"
-#include "Truck.h"
-#include "Transport.h"
-#include "GasStation.h"
-#include "Plane.h"
 
+class A {
+public:
+	int value1;
+	virtual void test() {
+		std::cout << "test A" << std::endl;
+	}
+};
+
+class B : public A {
+public:
+	int value2;
+	void test() override {
+		std::cout << "test B" << std::endl;
+	}
+};
 
 int main() {
-	Car car1{ 45 }, car2{ 50 }, car3{ 55 };
-	Bus bus{ 350 };
-	Truck truck1{ 550 }, truck2{ 450 };
-	Plane plane{ 1500 };
+	A* pointer = new B();
 
-	Transport transports[] = {car1, car2, car3, bus, truck1, truck2, plane};
-
-	GasStation station;
-
-	int total = station.calcTotalGas(transports, 7);
-
-	std::cout << "Total fuel is " << total << ".\n";
+	pointer->test();
 
 	return 0;
 }
